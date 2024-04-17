a500d0f777fc0a21663fb39a82902a50c2b34cd6
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-04-17 11:25:15 +0200

    end of day (not a lot been done)

--- app/server.js
+++ app/server.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const userRoute = require('./routes/User');
-//const authRoute = require('./routes/Auth');
+const authRoute = require('./routes/Auth');
+
+app.use(express.json()); 
 
 app.use('/user', userRoute);
-//app.use('/login', authRoute);
+app.use('/login', authRoute);
 
 app.listen(8080, () => {
     console.log('Server running on port 8080');
