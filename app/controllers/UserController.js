f7581daa5a4f124af0db1cc199f0143b1235fcfd
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-05-01 11:25:30 +0200

    end of day (login working)

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,62 +1,80 @@
-const mysql = require('mysql2');
-const crypto = require('crypto');
-const userModel = require('../models/userModel');
-
+const mysql = require("mysql2");
+const crypto = require("crypto");
+const userModel = require("../models/userModel");
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'db_183',
-  port: 6033
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "db_183",
+  port: 6033,
 };
 const connection = mysql.createConnection(dbConfig);
 
 exports.getUserProfile = (req, res) => {
+  const userId = req.session.userId;
   const username = req.params.username;
+
+  // Vérifier si l'utilisateur est connecté
+  if (!userId) {
+    res.status(401).json({ error: "Vous devez vous connecter" });
+    return;
+  }
+
   const query = `SELECT * FROM t_users WHERE username = ?`;
   connection.query(query, [username], (err, results) => {
+    // Vérifier si le profil demandé appartient à l'utilisateur connecté
+    if (userId !== results[0].id) {
+      res.status(403).json({
+        error: "Vous devez être connecté à ce compte pour le consulter",
+      });
+      return;
+    } else if (results.length === 0) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
       return;
     }
-    if (results.length === 0) {
-      res.status(404).json({ error: 'User not found' });
-      return;
-    }
+
     const userProfile = results[0];
     res.json(userProfile);
   });
 };
 
 exports.loginUser = (req, res) => {
-    const { username, password } = req.body;
-    
-    // Vérifier si l'utilisateur existe
-    userModel.getUserByUsername(username, (err, user) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
-        return;
-      }
-      if (!user) {
-        res.status(401).json({ error: 'User not found' });
-        return;
-      }
-  
-      // Vérifier le mot de passe
-      const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-      if (user.password !== hashedPassword) {
-        res.status(401).json({ error: 'Invalid username or password' });
-        return;
-      }
-  
-      // Authentification réussie
-      res.json({ message: 'Login successful' });
-    });
-  };
-  
+  const { username, password } = req.body;
+
+  // Vérifier si l'utilisateur existe
+  userModel.getUserByUsername(username, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!user) {
+      res.status(401).json({ error: "User not found" });
+      return;
+    }
+
+    // Vérifier le mot de passe
+    const hashedPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+    if (user.password !== hashedPassword) {
+      res.status(401).json({ error: "Invalid username or password" });
+      return;
+    } else {
+      req.session.userId = user.id;
+    }
+
+    // Authentification réussie
+    res.json({ message: "Login successful" });
+  });
+};
 
 exports.searchUsers = (req, res) => {
   const { name } = req.query;
@@ -64,7 +82,7 @@ exports.searchUsers = (req, res) => {
   connection.query(query, [`%${name}%`], (err, results) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
       return;
     }
     res.json(results);
@@ -72,32 +90,35 @@ exports.searchUsers = (req, res) => {
 };
 
 exports.signupUser = (req, res) => {
-    const { username, password } = req.body;
-  
-    // Vérifier si l'utilisateur existe déjà
-    userModel.getUserByUsername(username, (err, existingUser) => {
+  const { username, password } = req.body;
+
+  // Vérifier si l'utilisateur existe déjà
+  userModel.getUserByUsername(username, (err, existingUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (existingUser) {
+      res.status(400).json({ error: "Username already exists" });
+      return;
+    }
+
+    // Hasher le mot de passe
+    const hashedPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+
+    // Créer un nouvel utilisateur
+    const newUser = { username, password: hashedPassword };
+    userModel.saveUser(newUser, (err, result) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: "Internal server error" });
         return;
       }
-      if (existingUser) {
-        res.status(400).json({ error: 'Username already exists' });
-        return;
-      }
-  
-      // Hasher le mot de passe
-      const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
-  
-      // Créer un nouvel utilisateur
-      const newUser = { username, password: hashedPassword };
-      userModel.saveUser(newUser, (err, result) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ error: 'Internal server error' });
-          return;
-        }
-        res.json({ message: 'Signup successful' });
-      });
+      res.json({ message: "Signup successful" });
     });
-  };
+  });
+};
--- app/server.js
+++ app/server.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require("./routes/userRoutes");
+
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(userRoutes);
 
-
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
