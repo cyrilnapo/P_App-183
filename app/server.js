71837ceaaab68cce1ddb19a5cc014850e0e3f3ac
Cyril Napoleone <napoleone.cyril@gmail.com>
2024-05-01 09:18:03 +0200

    right routes + JDT update

--- app/server.js
+++ app/server.js
@@ -4,7 +4,8 @@ const app = express();
 const userRoutes = require('./routes/userRoutes');
 
 app.use(bodyParser.json());
-app.use('/users', userRoutes);
+app.use(userRoutes);
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
